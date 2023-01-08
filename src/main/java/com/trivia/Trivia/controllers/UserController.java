8812772c52a2f5ffbde150d023e9790dc25d96af
Jake Butler <JakeButler.student@careerdevs.com>
2023-01-08 16:11:13 -0500

    Added getAll to UserController

--- src/main/java/com/trivia/Trivia/controllers/UserController.java
+++ src/main/java/com/trivia/Trivia/controllers/UserController.java
@@ -5,10 +5,9 @@ import com.trivia.Trivia.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,5 +20,11 @@ public class UserController {
     public ResponseEntity<?> createUser(@RequestBody User newUser) {
         User createdUser = userRepository.save(newUser);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllUsers() {
+        List<User> allUsers = userRepository.findAll();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
     }
 }
