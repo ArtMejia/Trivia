fbe65b97bbd801d28e2754984bc74d4ffeeda996
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-16 19:46:06 -0500

    Updating and Delete Admin By Id

--- src/main/java/com/trivia/Trivia/controllers/AdminController.java
+++ src/main/java/com/trivia/Trivia/controllers/AdminController.java
@@ -1,11 +1,13 @@
 package com.trivia.Trivia.controllers;
 
+import com.sun.net.httpserver.HttpsServer;
 import com.trivia.Trivia.models.Admin;
 import com.trivia.Trivia.repositories.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -30,5 +32,31 @@ public class AdminController {
         List<Admin> allAdmins = adminRepository.findAll();
 
         return new ResponseEntity<>(allAdmins, HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Admin> updateAdmin(@PathVariable String id, @RequestBody Admin updateAdminData) {
+        Admin adminUpdate = adminRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+
+        if(!updateAdminData.getPassword().equals("")){
+            adminUpdate.setPassword((updateAdminData.getPassword()));
+        }
+
+        adminRepository.save(adminUpdate);
+
+        return new ResponseEntity<>(adminUpdate, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Admin> deleteAdmin(@PathVariable String id) {
+        Admin adminRequest = adminRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+
+        adminRepository.deleteById(id);
+
+        return new ResponseEntity<>(adminRequest, HttpStatus.OK);
     }
 }
